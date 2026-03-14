8cb92c030d7d064e32cc6abe37a0ac462309484c
nonahachi <samsungph558@gmail.com>
2026-03-14 17:28:59 +0100

    Add helper information about the application

---
+++ src/AppInfo.java
@@ -0,0 +1,9 @@
+package src;
+
+public class AppInfo {
+    public static void display() {
+        System.out.println("PGO Git Exercise v1.0");
+        System.out.println("Author: [Your Name]");
+        System.out.println("Status: Feature/Info branch merged.");
+    }
+}
